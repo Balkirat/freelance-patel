172f2ef4de56a16e12d060b81f3b02b250f47234
Balkirat <cheemaballu@gmail.com>
2020-08-04 17:13:50 -0400

    added animation to hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "./Hero.scss";
 import backgroundImage from "../../assets/images/heroImg.jpg";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__container">
         <img className="hero__background" src={backgroundImage}></img>
-        <div className="hero__info">
-          <h1 className="hero__info--heading">EXCELLENT <span>VISION</span> CARE</h1>
-          <h3 className="hero__info--subheading">We Provide Best Customer Services In GTA</h3>
-        </div>
+        <motion.div
+          className="hero__info"
+          initial={{ x: "-10vw", opacity: 0 }}
+          animate={{ x: "20%", opacity: 1 }}
+          transition={{
+            duration: 2.5,
+            type: "spring",
+            stiffness: 120,
+            delay: 1,
+          }}
+        >
+          <h1 className="hero__info--heading">
+            EXCELLENT <span>VISION</span> CARE
+          </h1>
+          <h3 className="hero__info--subheading">
+            We Provide Best Customer Services In GTA
+          </h3>
+        </motion.div>
       </div>
     </div>
   );
