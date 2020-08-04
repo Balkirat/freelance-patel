c78320bbe6e34968de78a3831d4d898f23f09f7c
Balkirat <cheemaballu@gmail.com>
2020-08-04 16:25:18 -0400

    hero component phase 1 done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,17 @@ import "./Hero.scss";
 import backgroundImage from "../../assets/images/heroImg.jpg";
 
 const Hero = () => {
-    return(
-        <div className="hero">
-            <img className="hero__background" src={backgroundImage}></img>
+  return (
+    <div className="hero">
+      <div className="hero__container">
+        <img className="hero__background" src={backgroundImage}></img>
+        <div className="hero__info">
+          <h1 className="hero__info--heading">EXCELLENT <span>VISION</span> CARE</h1>
+          <h3 className="hero__info--subheading">We Provide Best Customer Services In GTA</h3>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
